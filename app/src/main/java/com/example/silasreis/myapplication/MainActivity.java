35e5ab0869f10d2b364b53f9a82ac91e2a94c83f
silasreis <silas.1992@live.com>
2018-09-24 23:02:48 -0300

    Screenshot e share

--- app/src/main/java/com/example/silasreis/myapplication/MainActivity.java
+++ app/src/main/java/com/example/silasreis/myapplication/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.silasreis.myapplication;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
@@ -12,17 +19,25 @@ import com.google.android.gms.ads.reward.RewardItem;
 import com.google.android.gms.ads.reward.RewardedVideoAd;
 import com.google.android.gms.ads.reward.RewardedVideoAdListener;
 
-public class MainActivity extends AppCompatActivity implements RewardedVideoAdListener {
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements RewardedVideoAdListener, View.OnClickListener {
 
     private RewardedVideoAd mAd;
     private TextView mText;
     int moeda;
     AdView mAdview;
+    private Button fullPageScreenshot, customPageScreenshot;
+    private LinearLayout rootContent;
+    private ImageView imageView;
+    private TextView hiddenText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+        implementClickEvents();
 
         mText = (TextView)findViewById(R.id.textView);
 
@@ -40,6 +55,85 @@ public class MainActivity extends AppCompatActivity implements RewardedVideoAdLi
 
         loadRewardVideoAd();
 
+    }
+
+    private void findViews() {
+        fullPageScreenshot = (Button) findViewById(R.id.full_page_screenshot);
+        customPageScreenshot = (Button) findViewById(R.id.custom_page_screenshot);
+
+        rootContent = (LinearLayout) findViewById(R.id.root_content);
+
+        imageView = (ImageView) findViewById(R.id.image_view);
+
+        hiddenText = (TextView) findViewById(R.id.hidden_text);
+    }
+
+    /*  Implement Click events over Buttons */
+    private void implementClickEvents() {
+        fullPageScreenshot.setOnClickListener(this);
+        customPageScreenshot.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.full_page_screenshot:
+                takeScreenshot(ScreenshotType.FULL);
+                break;
+            case R.id.custom_page_screenshot:
+                takeScreenshot(ScreenshotType.CUSTOM);
+                break;
+        }
+    }
+
+    /*  Method which will take screenshot on Basis of Screenshot Type ENUM  */
+    private void takeScreenshot(ScreenshotType screenshotType) {
+        Bitmap b = null;
+        switch (screenshotType) {
+            case FULL:
+                //If Screenshot type is FULL take full page screenshot i.e our root content.
+                b = ScreenshotUtils.getScreenShot(rootContent);
+                break;
+            case CUSTOM:
+                //If Screenshot type is CUSTOM
+
+                fullPageScreenshot.setVisibility(View.INVISIBLE);//set the visibility to INVISIBLE of first button
+                hiddenText.setVisibility(View.VISIBLE);//set the visibility to VISIBLE of hidden text
+
+                b = ScreenshotUtils.getScreenShot(rootContent);
+
+                //After taking screenshot reset the button and view again
+                fullPageScreenshot.setVisibility(View.VISIBLE);//set the visibility to VISIBLE of first button again
+                hiddenText.setVisibility(View.INVISIBLE);//set the visibility to INVISIBLE of hidden text
+
+                break;
+        }
+
+        //If bitmap is not null
+        if (b != null) {
+            showScreenShotImage(b);
+
+            File saveFile = ScreenshotUtils.getMainDirectoryName(this);
+            File file = ScreenshotUtils.store(b, "screenshot" + screenshotType + ".jpg", saveFile);//save the screenshot to selected path
+            shareScreenshot(file);
+        } else
+            Toast.makeText(this, R.string.screenshot_take_failed, Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void showScreenShotImage(Bitmap b) {
+        imageView.setImageBitmap(b);
+    }
+
+    private void shareScreenshot(File file) {
+        Uri uri = Uri.fromFile(file);//Convert file path into Uri for sharing
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.setType("image/*");
+        intent.putExtra(android.content.Intent.EXTRA_SUBJECT, "");
+        intent.putExtra(android.content.Intent.EXTRA_TEXT, getString(R.string.sharing_text));
+        intent.putExtra(Intent.EXTRA_STREAM, uri);
+        startActivity(Intent.createChooser(intent, getString(R.string.share_title)));
     }
 
     private void  loadRewardVideoAd()
